656ed8a839875075aeccc8d3bfb88fc9055e234f
Ramling855 <ramling855@gmail.com>
2022-05-20 01:30:38 +0530

    1 file modified

--- src/Show.jsx
+++ src/Show.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React,{useEffect,useState} from "react";
 import "./style.css";
 import {put,add} from './seatSlice';
 import {useSelector,useDispatch}from 'react-redux'
@@ -8,28 +8,36 @@ import{ Link} from 'react-router-dom'
 
 
 export default function Show() {
+  const[match,setMatch]=useState()
   const dispatch=useDispatch();
-
-
+  const seat=useSelector((state)=>state.sea.data)
+  const[out,setOut]=useState([])
 const Payment=(all)=>{
   // dispatch(put(all))
 dispatch(put(all.seatNo))
+setMatch(all.seatNo);
+// if(match!==all.seatNo){out.push(seat)}
 }
 
+useEffect(()=>{
+  
 
-  const seat=useSelector((state)=>state.sea.data)
+},[])
+
  
-// console.log(datta.data)
+  const co=useSelector((state)=>state.sea.seat)
+  
   return (
     <div>
        <Timerj/>
      
-{seat.map((all)=>{
-return <li style={{margin:'30%'}}><h1>{all.seatNo}</h1>
+{seat.map((all,i)=>{return <li key={i} style={{margin:'30%',display:'inline'}}><h1>{all.seatNo}</h1>
 <h1>{all.img}</h1> 
 <h1>{all.Price}</h1>
 <Link to='/Success'><button onClick={()=>Payment(all)} >Proced to paymnt</button></Link></li>
-})}<br/>
+}
+
+)}<br/>
 
 
 
